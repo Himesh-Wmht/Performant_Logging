fde62facb949e656fb91f922e1cf467ef0dc88ed
Dinuka Kulathunga <dinukakulatunga@gmail.com>
2023-07-01 22:07:21 +0530

    Add project files.

---
+++ ServiceWorkerTest/Log.cs
@@ -0,0 +1,8 @@
+﻿namespace Performant_Logging
+{
+    public static partial class Log
+    {
+        [LoggerMessage(20, LogLevel.Information, "Weather forecast requested at {date}.")]
+        public static partial void WeatherForcastRequested(this ILogger logger, DateTime date);
+    }
+}
